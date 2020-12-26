c263c492e79b9056b5a6f65847af29b611f9dd08
Albert-BCRP <54189489+Albert-BCRP@users.noreply.github.com>
2020-12-26 11:34:16 -0500

    Update models.py

--- models.py
+++ models.py
@@ -9,61 +9,75 @@ from otree.api import (
     currency_range,
 )
 
-
+import random
 doc = """
-Este es un juego de 10 rondas sobre elección de niveles de prevención con 10 jugadores.
+This is a ten period pandemic-response game
 """
 
 
 class Constants(BaseConstants):
-    name_in_url = 'prevention_level'
-    players_per_group = 10
+    name_in_url = 'pandemia'
+    players_per_group = 3
     num_rounds = 10
-
-    introduction_template = 'prevention_level/Introduction.html'
-    general_instructions = 'prevention_level/GeneralInstructions.html'
-    specific_instructions = 'prevention_level/SpecificInstructions.html'
-
-    # """Amount allocated to each player"""
     endowment = c(150)
-    multiplier = 50
-
+    instructions_template = 'pandemia/instructions.html'
+    prob_intrinseca = int(20)
+    prob_contagio = float(0.4)
 
 class Subsession(BaseSubsession):
+
+    def creating_session(self):
+
+        for p in self.get_players():
+            if self.round_number == 1:
+                p.prob_intrinseca = int(random.randint(c(5),c(50)))
+            else:
+                p.prob_intrinseca = p.in_round(1).prob_intrinseca
+
+
     def vars_for_admin_report(self):
-        contributions = [
-            p.contribution for p in self.get_players() if p.contribution != None
+        precautions = [
+            p.precaution for p in self.get_players() if p.precaution != None
         ]
-        if contributions:
+        if precautions:
             return dict(
-                avg_contribution=sum(contributions) / len(contributions),
-                min_contribution=min(contributions),
-                max_contribution=max(contributions),
+                avg_precaution=sum(precautions) / len(precautions),
+                min_precaution=min(precautions),
+                max_precaution=max(precautions),
             )
         else:
             return dict(
-                avg_contribution='(no data)',
-                min_contribution='(no data)',
-                max_contribution='(no data)',
+                avg_precaution='(no data)',
+                min_precaution='(no data)',
+                max_precaution='(no data)',
             )
 
 
 class Group(BaseGroup):
-    total_contribution = models.CurrencyField()
+    mean_precaution = models.FloatField()
 
-    individual_share = models.CurrencyField()
+    individual_share = models.FloatField()
 
     def set_payoffs(self):
-        self.total_contribution = sum([p.contribution for p in self.get_players()])
+        self.mean_precaution = sum([p.precaution for p in self.get_players()])/Constants.players_per_group
         self.individual_share = (
-            self.total_contribution * Constants.multiplier / Constants.players_per_group
+            0.6/ Constants.players_per_group
         )
         for p in self.get_players():
-            p.payoff = (Constants.endowment - p.contribution) + self.individual_share
+            p.payoff = (Constants.endowment - c(p.precaution) * c(p.precaution))
+            p.prob_contagio = p.prob_intrinseca + (5 - 0.4 * p.precaution - 0.6 * self.mean_precaution) * 10
+
+
+
 
 
 class Player(BasePlayer):
-    contribution = models.CurrencyField(
-        min=0, max=Constants.endowment, doc="""The amount contributed by the player""",
-        label="How much will you contribute to the project (from 0 to 100)?"
+
+    prob_intrinseca = models.IntegerField()
+    precaution = models.IntegerField(
+        min=0, max=5, doc="""The level of precaution taken by the player""",
+        label="How much precaution will you choose to take (from 0 to 5)?" ,
+        choices = [0, 1, 2, 3, 4, 5]
     )
+    prob_contagio = models.FloatField()
+    contagiado = models.IntegerField()
