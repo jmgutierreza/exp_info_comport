6277f5b727d68b6d35cb74374400cf3f79f034d0
Albert-BCRP <54189489+Albert-BCRP@users.noreply.github.com>
2021-01-08 16:05:42 -0500

    Update pages.py

--- pages.py
+++ pages.py
@@ -7,11 +7,15 @@ class Welcome(Page):
     """Welcome page"""
     def is_displayed(self):
         return self.round_number == 1
+    def vars_for_template(self):
+        return dict(participant_id = self.participant.label)
 
 class Introduction(Page):
     """Description of the game"""
     def is_displayed(self):
         return self.round_number == 1
+    def vars_for_template(self):
+        return dict(participant_id = self.participant.label)
     pass
 
 
@@ -19,12 +23,16 @@ class Game(Page):
     """How to play and returns expected"""
     def is_displayed(self):
         return self.round_number == 1
+    def vars_for_template(self):
+        return dict(participant_id = self.participant.label)
     pass
 
 class Probab(Page):
     """How affect the prevention level"""
     def is_displayed(self):
         return self.round_number == 1
+    def vars_for_template(self):
+        return dict(participant_id = self.participant.label)
     pass
 
 class Contribute(Page):
@@ -32,43 +40,53 @@ class Contribute(Page):
 
     form_model = 'player'
     form_fields = ['precaution']
-
+    def vars_for_template(self):
+        return dict(participant_id = self.participant.label)
 
 class ResultsWaitPage(WaitPage):
     after_all_players_arrive = 'set_payoffs'
 
     body_text = "Waiting for other participants to decide."
+    def vars_for_template(self):
+        return dict(participant_id = self.participant.label)
 
 
 class Results(Page):
     """Players payoff: How much each has earned"""
 
     def vars_for_template(self):
-        return dict(costo_contagiado=self.player.prob_intrinseca)
+        return dict(costo_contagiado=self.player.prob_intrinseca ,
+                    participant_id = self.participant.label)
 
 class R_Final(Page):
     def is_displayed(self):
         return self.round_number == 10
     def vars_for_template(self):
-        return dict(pago_real = self.player.pago_acumulado.to_real_world_currency(self.session))
+        return dict(pago_real = self.player.pago_acumulado.to_real_world_currency(self.session) ,
+               participant_id = self.participant.label)
 
 class Treatment_1(Page):
     """Just for treated group 1"""
     def is_displayed(self):
         return self.player.tratado_1 == 1 and self.round_number == 1
-        #return self.round_number == 1
 
+    def vars_for_template(self):
+        return dict(participant_id=self.participant.label)
 
 class Treatment_2(Page):
     """"Just for treated group 2"""
     def is_displayed(self):
         return self.player.tratado_1 == 2 and self.round_number == 1
+        return dict(participant_id=self.participant.label)
+
+    def vars_for_template(self):
+        return dict(participant_id=self.participant.label)
         #return self.round_number == 1
 
 class Last(Page):
     def is_displayed(self):
         return self.round_number == 10
     def vars_for_template(self):
-        return dict(participant_id = self.player.id_in_group)
+        return dict(participant_id = self.participant.label)
 
 page_sequence = [Welcome, Introduction, Game, Probab, Contribute, ResultsWaitPage, Results, Treatment_1, Treatment_2, R_Final, Last]
